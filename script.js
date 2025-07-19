398ddda9d7b464ebffb64ee4fb63a76f00d11ec2
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-19 16:27:36 +0200

    lamp-on when you click

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const element = document.querySelector('img')
+const light_btn = document.querySelector('button')
 
+
+function switcher(object) {
+   return object.src='./img/yellow_lamp.png'
+}
+
+
+light_btn.addEventListener('click', () => switcher(element))
