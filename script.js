03eb00c96cc21f4b5ce4cdf445f509e4a4ae7859
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-20 15:10:07 +0200

    Bonus

--- script.js
+++ script.js
@@ -3,8 +3,24 @@ const light_btn = document.querySelector('button')
 
 
 function switcher(object) {
-   return object.src='./img/yellow_lamp.png'
+    if (object.src.includes('yellow_lamp.png')) {
+         object.src='./img/white_lamp.png'
+    }
+    else if (object.src.includes('white_lamp.png')) {
+         object.src='./img/yellow_lamp.png'
+    }
+}
+
+function change_btn(btn) {
+    if (btn.innerHTML.includes('Accendi')) {
+        btn.innerHTML = 'Spegni'
+    }
+    else if (btn.innerHTML.includes('Spegni')) {
+        btn.innerHTML = 'Accendi'
+    }
 }
 
 
+
 light_btn.addEventListener('click', () => switcher(element))
+light_btn.addEventListener('click', () => change_btn(light_btn))
