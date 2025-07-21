04e878641b3562df4bdc4ae802ab3a3452a83423
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-21 19:10:48 +0200

    some css and rewrote the js script with a single function

--- script.js
+++ script.js
@@ -2,25 +2,18 @@ const element = document.querySelector('img')
 const light_btn = document.querySelector('button')
 
 
-function switcher(object) {
-    if (object.src.includes('yellow_lamp.png')) {
-         object.src='./img/white_lamp.png'
+function switcher(lamp, btn) {
+    if (lamp.src.includes('yellow_lamp.png') && btn.innerHTML.includes('Spegni')) {
+         lamp.src='./img/white_lamp.png'
+         btn.innerHTML = 'Accendi'
     }
-    else if (object.src.includes('white_lamp.png')) {
-         object.src='./img/yellow_lamp.png'
-    }
-}
-
-function change_btn(btn) {
-    if (btn.innerHTML.includes('Accendi')) {
-        btn.innerHTML = 'Spegni'
-    }
-    else if (btn.innerHTML.includes('Spegni')) {
-        btn.innerHTML = 'Accendi'
+    else if (lamp.src.includes('white_lamp.png') && btn.innerHTML.includes('Accendi')) {
+         lamp.src='./img/yellow_lamp.png'
+         btn.innerHTML = 'Spegni'
     }
 }
 
 
 
-light_btn.addEventListener('click', () => switcher(element))
-light_btn.addEventListener('click', () => change_btn(light_btn))
+light_btn.addEventListener('click', () => switcher(element, light_btn))
+
